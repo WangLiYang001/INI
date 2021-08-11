65b7e96331f98dfaeb9f20e019865bf1590dd20e
WLY001 <2574934365@qq.com>
2021-08-11 11:09:02 +0800

    删除了两个文件

--- PBCSystem/Assets/Scripts/Data/HLocalAdminCacheDefine.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class HLocalAdminCacheDefine {
-
-    internal static string _cacheUserKey = "IceWorld_Key_User" + Application.version;
-}
